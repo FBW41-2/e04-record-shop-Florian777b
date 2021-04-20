5e8f65e4e969e9cc24ba726ade5fc1fd53d02213
Florian <bocu.alexandru@gmail.com>
2021-04-20 16:36:14 +0200

    low db inserted

--- routes/index.js
+++ routes/index.js
@@ -1,37 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
+const adapter = new FileSync('./data/db.json')
+const db = low(adapter)
+router.get('/api/records', (req, res, next) => {
+    res.json(db)
+})
+router.post('/api/records', (req, res, next) => {
+    db.push({
+        "name": req.body.name,
+        "title": req.body.title,
+        "year": req.body.year
+        })
+        .write()
+    res.redirect('/api/records')
+})
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-
-
-const records = [{
-  "name": "Linkin park",
-  "title": "Unforgiven",
-  "year": "1994"
-},
-
-  {"name": "Beatles",
-  "title": "Let it be",
-  "year": "1975"
-},
-]
-
-router.get('/api/records', function(req, res,) {
-  res.json(records)
-})
-
-router.post('/api/records', (req, res,) => {
-  records.push({
-    name: req.body.name,
-    title: req.body.title,
-    year: req.body.year,
-  });
-  res.redirect("/api/records")
-});
-
-
-
 module.exports = router;
