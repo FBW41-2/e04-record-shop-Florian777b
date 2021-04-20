d8237d2bba0dcb7f2a3d97d282b27488acd98fdd
Florian <bocu.alexandru@gmail.com>
2021-04-20 14:25:48 +0200

    first task finished

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,32 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+
+const records = [{
+  "name": "Linkin park",
+  "title": "Unforgiven",
+  "year": "1994"
+},
+
+  {"name": "Beatles",
+  "title": "Let it be",
+  "year": "1975"
+},
+]
+
+router.get('/api/records', function(req, res,) {
+  res.json(records)
+})
+
+router.post('/api/records', (req, res,) => {
+  records.push({
+    name: req.body.name,
+    title: req.body.title,
+    year: req.body.year,
+  });
+  res.redirect("/api/records")
+});
+
+
+
 module.exports = router;
